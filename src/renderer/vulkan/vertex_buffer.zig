38a290008d8339dbdac9e9710c7af0699159ce8e
LDprg <lukas_4dr@gmx.at>
2024-12-04 12:35:21 +0100

    deduplicate vertex buffer code

--- src/renderer/vulkan/vertex_buffer.zig
+++ src/renderer/vulkan/vertex_buffer.zig
@@ -52,13 +52,65 @@ pub const Vertex = struct {
 pub var vertices: []Vertex = undefined;
 pub var indices: []u16 = undefined;
 
+pub fn init() !void {
+    var vert = [_]Vertex{
+        Vertex{ .pos = [2]f32{ 100, 100 }, .color = [4]f32{ 1, 0, 0, 1 } },
+        Vertex{ .pos = [2]f32{ 500, 100 }, .color = [4]f32{ 0, 1, 0, 1 } },
+        Vertex{ .pos = [2]f32{ 500, 500 }, .color = [4]f32{ 0, 0, 1, 1 } },
+        Vertex{ .pos = [2]f32{ 100, 500 }, .color = [4]f32{ 1, 0, 1, 1 } },
+    };
+    vertices = vert[0..];
+
+    var ind = [_]u16{ 0, 1, 2, 2, 3, 0 };
+    indices = ind[0..];
+
+    const buffer_size: c.VkDeviceSize = @sizeOf(Vertex) * vertices.len;
+    try createBufferStaging(&vertex_buffer, &vertex_buffer_alloc, Vertex, vertices, c.VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, buffer_size);
+    try createBufferStaging(&index_buffer, &index_buffer_alloc, u16, indices, c.VK_BUFFER_USAGE_INDEX_BUFFER_BIT, buffer_size);
+}
+
+pub fn deinit() void {
+    c.vmaDestroyBuffer(allocator.allocator, vertex_buffer, vertex_buffer_alloc);
+    c.vmaDestroyBuffer(allocator.allocator, index_buffer, index_buffer_alloc);
+}
+
+fn createBufferStaging(buffer: *c.VkBuffer, buffer_alloc: *c.VmaAllocation, comptime result_type: type, result: []result_type, flags: c.VmaAllocationCreateFlags, size: c.VkDeviceSize) !void {
+    var staging_buffer: c.VkBuffer = null;
+    var staging_buffer_alloc: c.VmaAllocation = null;
+    var staging_buffer_alloc_info = c.VmaAllocationInfo{};
+
+    try createBuffer(
+        size,
+        c.VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+        c.VMA_ALLOCATION_CREATE_HOST_ACCESS_SEQUENTIAL_WRITE_BIT | c.VMA_ALLOCATION_CREATE_MAPPED_BIT,
+        &staging_buffer,
+        &staging_buffer_alloc,
+        &staging_buffer_alloc_info,
+    );
+
+    @memcpy(@as([*]result_type, @ptrCast(@alignCast(staging_buffer_alloc_info.pMappedData))), result);
+
+    try createBuffer(
+        size,
+        @as(u32, @intCast(c.VK_BUFFER_USAGE_TRANSFER_DST_BIT)) | flags,
+        0,
+        buffer,
+        buffer_alloc,
+        null,
+    );
+
+    try copyBuffer(staging_buffer, buffer.*, size);
+
+    c.vmaDestroyBuffer(allocator.allocator, staging_buffer, staging_buffer_alloc);
+}
+
 pub fn createBuffer(
     size: c.VkDeviceSize,
     usage: c.VkBufferUsageFlags,
     flags: c.VmaAllocationCreateFlags,
     buffer: *c.VkBuffer,
-    bufferAlloc: *c.VmaAllocation,
-    bufferAllocInfo: ?*c.VmaAllocationInfo,
+    buffer_alloc: *c.VmaAllocation,
+    buffer_alloc_info: ?*c.VmaAllocationInfo,
 ) !void {
     const buffer_info = c.VkBufferCreateInfo{
         .sType = c.VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO,
@@ -72,10 +124,10 @@ pub fn createBuffer(
         .flags = flags,
     };
 
-    try util.check_vk(c.vmaCreateBuffer(allocator.allocator, &buffer_info, &alloc_create_info, buffer, bufferAlloc, bufferAllocInfo));
+    try util.check_vk(c.vmaCreateBuffer(allocator.allocator, &buffer_info, &alloc_create_info, buffer, buffer_alloc, buffer_alloc_info));
 }
 
-fn copyBuffer(srcBuffer: c.VkBuffer, dstBuffer: c.VkBuffer, size: c.VkDeviceSize) !void {
+fn copyBuffer(src_buffer: c.VkBuffer, dst_buffer: c.VkBuffer, size: c.VkDeviceSize) !void {
     const alloc_info = c.VkCommandBufferAllocateInfo{
         .sType = c.VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO,
         .level = c.VK_COMMAND_BUFFER_LEVEL_PRIMARY,
@@ -98,7 +150,7 @@ fn copyBuffer(srcBuffer: c.VkBuffer, dstBuffer: c.VkBuffer, size: c.VkDeviceSize
         .dstOffset = 0,
         .size = size,
     };
-    c.vkCmdCopyBuffer(cmdbuff, srcBuffer, dstBuffer, 1, &copy_region);
+    c.vkCmdCopyBuffer(cmdbuff, src_buffer, dst_buffer, 1, &copy_region);
 
     try util.check_vk(c.vkEndCommandBuffer(cmdbuff));
 
@@ -112,89 +164,4 @@ fn copyBuffer(srcBuffer: c.VkBuffer, dstBuffer: c.VkBuffer, size: c.VkDeviceSize
     try util.check_vk(c.vkQueueWaitIdle(queue.graphics_queue));
 
     c.vkFreeCommandBuffers(device.device, command_buffer.command_pool, 1, &cmdbuff);
-}
-
-fn createVertexBuffer() !void {
-    const buffer_size: c.VkDeviceSize = @sizeOf(Vertex) * vertices.len;
-
-    var staging_buffer: c.VkBuffer = null;
-    var staging_buffer_alloc: c.VmaAllocation = null;
-    var staging_buffer_alloc_info = c.VmaAllocationInfo{};
-
-    try createBuffer(
-        buffer_size,
-        c.VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
-        c.VMA_ALLOCATION_CREATE_HOST_ACCESS_SEQUENTIAL_WRITE_BIT | c.VMA_ALLOCATION_CREATE_MAPPED_BIT,
-        &staging_buffer,
-        &staging_buffer_alloc,
-        &staging_buffer_alloc_info,
-    );
-
-    @memcpy(@as([*]Vertex, @ptrCast(@alignCast(staging_buffer_alloc_info.pMappedData))), vertices);
-
-    try createBuffer(
-        buffer_size,
-        c.VK_BUFFER_USAGE_TRANSFER_DST_BIT | c.VK_BUFFER_USAGE_VERTEX_BUFFER_BIT,
-        0,
-        &vertex_buffer,
-        &vertex_buffer_alloc,
-        null,
-    );
-
-    try copyBuffer(staging_buffer, vertex_buffer, buffer_size);
-
-    c.vmaDestroyBuffer(allocator.allocator, staging_buffer, staging_buffer_alloc);
-}
-
-fn createIndexBuffer() !void {
-    const buffer_size: c.VkDeviceSize = @sizeOf(Vertex) * vertices.len;
-
-    var staging_buffer: c.VkBuffer = null;
-    var staging_buffer_alloc: c.VmaAllocation = null;
-    var staging_buffer_alloc_info = c.VmaAllocationInfo{};
-
-    try createBuffer(
-        buffer_size,
-        c.VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
-        c.VMA_ALLOCATION_CREATE_HOST_ACCESS_SEQUENTIAL_WRITE_BIT | c.VMA_ALLOCATION_CREATE_MAPPED_BIT,
-        &staging_buffer,
-        &staging_buffer_alloc,
-        &staging_buffer_alloc_info,
-    );
-
-    @memcpy(@as([*]u16, @ptrCast(@alignCast(staging_buffer_alloc_info.pMappedData))), indices);
-
-    try createBuffer(
-        buffer_size,
-        c.VK_BUFFER_USAGE_TRANSFER_DST_BIT | c.VK_BUFFER_USAGE_INDEX_BUFFER_BIT,
-        0,
-        &index_buffer,
-        &index_buffer_alloc,
-        null,
-    );
-
-    try copyBuffer(staging_buffer, index_buffer, buffer_size);
-
-    c.vmaDestroyBuffer(allocator.allocator, staging_buffer, staging_buffer_alloc);
-}
-
-pub fn init() !void {
-    var vert = [_]Vertex{
-        Vertex{ .pos = [2]f32{ 100, 100 }, .color = [4]f32{ 1, 0, 0, 1 } },
-        Vertex{ .pos = [2]f32{ 500, 100 }, .color = [4]f32{ 0, 1, 0, 1 } },
-        Vertex{ .pos = [2]f32{ 500, 500 }, .color = [4]f32{ 0, 0, 1, 1 } },
-        Vertex{ .pos = [2]f32{ 100, 500 }, .color = [4]f32{ 1, 0, 1, 1 } },
-    };
-    vertices = vert[0..];
-
-    var ind = [_]u16{ 0, 1, 2, 2, 3, 0 };
-    indices = ind[0..];
-
-    try createVertexBuffer();
-    try createIndexBuffer();
-}
-
-pub fn deinit() void {
-    c.vmaDestroyBuffer(allocator.allocator, vertex_buffer, vertex_buffer_alloc);
-    c.vmaDestroyBuffer(allocator.allocator, index_buffer, index_buffer_alloc);
 }
