58812a94fda0fab7d97629299d0ceb3050684e58
LDprg <lukas_4dr@gmx.at>
2024-12-06 23:03:53 +0100

    add vale

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # UniNote
+UniNote is a note taking program for Students!
+It focuses on high drawing performance and student related features.
+To achieve this it uses SDL3 and VULKAN combined with ImGui.
+
+This project is early into development so do not expect much out of it, but feel free to contribute.
+
+# WARNING
+DO NOT USE THIS SOFTWARE IN PRODUCTION AND NOT EVEN ALPHA QUALITY
+AND THERE IS NOTHING LIKE COMPATIBILITY EVEN BETWEEN MINOR VERSIONS
--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,13 @@
-//! root source file just all modules and the main loop
+//! UniNote is a note taking program for Students!
+//! It focuses on high drawing performance and student related features.
+//! To achieve this it uses SDL3 and VULKAN combined with ImGui
+//! # WARNING
+//! DO NOT USE THIS SOFTWARE IN PRODUCTION AND NOT EVEN ALPHA QUALITY
+//! AND THERE IS NOTHING LIKE COMPATIBILITY EVEN BETWEEN MINOR VERSIONS
 
 const std = @import("std");
 
+// Include all modules so they can be accessed using: `@import("root")`
 pub const c = @import("c.zig");
 
 pub const app = @import("app.zig");
